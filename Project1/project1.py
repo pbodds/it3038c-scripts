3a84be1568c3116ea726dadabb4b307013d50005
pbodds <boddypm@mail.uc.edu>
2022-10-09 15:22:42 -0400

    Project 1

--- Project1/project1.py
+++ Project1/project1.py
@@ -1,9 +1,12 @@
 import shutil
 import time
 
+# First part of this script will allow the user to choose between a 24hr clock or a 12hr clock format. After, the total space of your hard drive will be listed along 
+# with the used space and free space.
+
 loop = True
 while loop == True:
-
+    try:
         print("Welcome to the disk checker script! To be considerate, do you want your time formatted in (1) 12 hours or (2) 24 hours?")
         selectedTime = int(input())
 
@@ -19,7 +22,9 @@ while loop == True:
         
         else:
             print("Please enter (1) for 12 hour time format or (2) for 24 hour time format")
-
+    except:
+        print("Please enter a number")
+        
 total, used, free = shutil.disk_usage("/")
 
 print("Time to check our hard drive space!")
