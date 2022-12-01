332fc5e93e611d0ad40d473ec7da67aa08a89b4c
pbodds <boddypm@mail.uc.edu>
2022-11-30 19:37:15 -0500

    commenting

--- project3/project3.py
+++ project3/project3.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 import lyricsgenius
 
-#Creating the flask app and setting the template folder
+#Creating the flask app and setting the template folder.
 app = Flask(__name__, template_folder='templates')
 
 #First route for my index page, localhost5000/. 
@@ -10,22 +10,23 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template("index.html")
 
-#Second route for my lyrics search result page, Localhost5000/lyric-search. 
+#Second route for my lyrics search result page, Localhost5000/lyric-search. Allows GET and POST methods.
 @app.route('/lyric-search/',  methods=['GET', 'POST'])
 def lyric_search():
-	#Creating variables for search parameters, pulled from index.html
+	#Creating variables for search parameters, pulled from index.html.
 	token = request.form['tokenName']
 	artist = request.form['artistName']
 	song = request.form['songName']
-	#Accepting the token for API calls to Genius
+	#Accepting the token for API calls to Genius.
 	genius = lyricsgenius.Genius(token)
 	genius.response_format = 'html'
-	#Search for an artist with a max of 2 songs
+	#Search for an artist with a max of 2 songs.
 	artistG = genius.search_artist(artist, max_songs=2, sort="title", include_features=True)
 	#When the artist is found, find the song that was given above as a request.  
 	songG = artistG.song(song)
 	#On the template lyric-search, pass lyrics to lyric-search. songG.Lyrics will output the song lyrics from song. 
 	return render_template("lyric-search.html", lyrics=songG.lyrics)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
